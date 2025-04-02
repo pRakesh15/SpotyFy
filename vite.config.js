f8b91c339b60ceaf10a6b98213c0ff28e570b2ca
yiconic <rp5865442@gmail.com>
2025-04-02 21:18:04 +0530

    fix build issue

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        quietDeps: true,  // Suppress warnings from dependencies
+      },
+    },
+  },
+  server: {
+    port: 5173
+  }
 })
